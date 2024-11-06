abf6db8dd89a105fa93f14e6b667a6640b56e830
Abdullah-BS <139412761+Abdullah-BS@users.noreply.github.com>
2024-11-06 14:21:23 +0300

    Update KSA_ANTHEM.ino

--- KSA_ANTHEM.ino
+++ KSA_ANTHEM.ino
@@ -122,15 +122,15 @@ int melody[] = {
 
   
   
-  // NOTE_F4,-4, NOTE_F4,8, NOTE_F4,-2, REST,2,   //سارعي
-  // NOTE_C4,4, NOTE_F4,-4, NOTE_G4,8, NOTE_A4,4, NOTE_AS4,4, NOTE_C5,-2, REST,4,         //للمجد والعلياء
-  // NOTE_C5,-4, NOTE_D5,8, NOTE_C5,4, NOTE_AS4,4, NOTE_A4,4, NOTE_G4,4, NOTE_F4,4, NOTE_E4,4, NOTE_F4,-1, REST,2,   //مجدي لخالق السماء
-  // NOTE_C5,-4, NOTE_C5,8, NOTE_C5,4, NOTE_D5,4, NOTE_C5,-4, NOTE_AS4,8, NOTE_A4,4, NOTE_F4,4,    //وارفع الخفاق الاخضر
-  // NOTE_C5,-4, NOTE_C5,8, NOTE_C5,4, NOTE_F5,4, NOTE_E5,-4, NOTE_D5,8, NOTE_C5,4, NOTE_C5,4,     //يحمل النور المسطر 
-  // NOTE_AS4,-4, NOTE_AS4,8, NOTE_AS4,4, NOTE_D5,4, NOTE_C5,-4, NOTE_AS4,8, NOTE_A4,4, NOTE_F4,4,    //رددي الله اكبر
-  // NOTE_C5,-4, REST,8, NOTE_D5,-4, NOTE_E5,8, NOTE_F5,-2, REST,-4,  //يا موطني
-  // NOTE_F4,-4, NOTE_F4,8, NOTE_F4,-1, REST,4, //موطني
-  // NOTE_C4,4, NOTE_F4,-4, NOTE_G4,8, NOTE_A4,4, NOTE_AS4,4, NOTE_C5,-4,  NOTE_C5,8, NOTE_C5,-4, REST,8, //عشت فخر المسلمين
+  NOTE_F4,-4, NOTE_F4,8, NOTE_F4,-2, REST,2,   //سارعي
+  NOTE_C4,4, NOTE_F4,-4, NOTE_G4,8, NOTE_A4,4, NOTE_AS4,4, NOTE_C5,-2, REST,4,         //للمجد والعلياء
+  NOTE_C5,-4, NOTE_D5,8, NOTE_C5,4, NOTE_AS4,4, NOTE_A4,4, NOTE_G4,4, NOTE_F4,4, NOTE_E4,4, NOTE_F4,-1, REST,2,   //مجدي لخالق السماء
+  NOTE_C5,-4, NOTE_C5,8, NOTE_C5,4, NOTE_D5,4, NOTE_C5,-4, NOTE_AS4,8, NOTE_A4,4, NOTE_F4,4,    //وارفع الخفاق الاخضر
+  NOTE_C5,-4, NOTE_C5,8, NOTE_C5,4, NOTE_F5,4, NOTE_E5,-4, NOTE_D5,8, NOTE_C5,4, NOTE_C5,4,     //يحمل النور المسطر 
+  NOTE_AS4,-4, NOTE_AS4,8, NOTE_AS4,4, NOTE_D5,4, NOTE_C5,-4, NOTE_AS4,8, NOTE_A4,4, NOTE_F4,4,    //رددي الله اكبر
+  NOTE_C5,-4, REST,8, NOTE_D5,-4, NOTE_E5,8, NOTE_F5,-2, REST,-4,  //يا موطني
+  NOTE_F4,-4, NOTE_F4,8, NOTE_F4,-1, REST,4, //موطني
+  NOTE_C4,4, NOTE_F4,-4, NOTE_G4,8, NOTE_A4,4, NOTE_AS4,4, NOTE_C5,-4,  NOTE_C5,8, NOTE_C5,-4, REST,8, //عشت فخر المسلمين
   NOTE_C5,-4, NOTE_D5,8, NOTE_C5,4, NOTE_AS4,4, NOTE_C5,-4, REST,8, NOTE_C5,-4, NOTE_C5,8, NOTE_F5,-4, REST,8, NOTE_C5,-4, NOTE_C5,8, NOTE_F5,-4, //عاش الملك للعلم والوطن
   REST,4, NOTE_C6,4, REST,8, NOTE_C6,8, NOTE_F6,-4,
 
